6a5c67b31384a956832920e1d2ff6dff407581fb
Maman <ingratarmanrizky19@gmail.com>
2026-05-26 11:04:15 +0700

    siwitch case 3

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -140,5 +140,10 @@ int main()
                 q.remove();
                 break;
             }
+            case '3':
+            {
+                q.display();
+                break;
+            }
     }
 }
